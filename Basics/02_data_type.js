cfc02e0762e66c5496531c7479014190f5e0a52b
Anjay kumar <79074339+Anjay29@users.noreply.github.com>
2023-10-12 14:06:22 +0000

    new commit

--- Basics/02_data_type.js
+++ Basics/02_data_type.js
@@ -4,3 +4,16 @@ var Name = "Anjay"
 college = "NIT"
 
 console.table([accountID,city,Name,college]);
+
+
+// use typeof function to check datatype
+// example
+const check = "Anjay";
+console.log(typeof check);
+// Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigin
