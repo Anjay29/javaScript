a150e3cb916ee33d9771849a995d2a61ad52e557
Anjay kumar <79074339+Anjay29@users.noreply.github.com>
2023-10-28 13:13:26 +0000

    fetch

--- promise/fetch.js
+++ promise/fetch.js
@@ -1,7 +1,11 @@
 const promiseFive = async () => {
-    const response = fetch('https://jsonplaceholder.org/users');
-    const data = response.json();
-    console.log(data);
+    try {
+        const response = await fetch('https://jsonplaceholder.org/users');
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.log("E: wrong");
+    }
 }
 
 promiseFive();
